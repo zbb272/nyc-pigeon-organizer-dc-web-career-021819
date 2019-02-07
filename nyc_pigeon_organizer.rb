e8486d347ee8ffbdf5a74aac1c6d39f1559e7a82
Zachary <zbberman@yahoo.com>
2019-02-07 23:22:30 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,44 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  
+  data.each do | top_level, top_data |
+    top_data.each do | attribute, name_array |
+      name_array.each do | name |
+        new_hash[name] = {}
+      end 
+    end 
+  end 
+  
+  new_hash.each do | name, empty_hash |
+    data.each do | attribute, value |
+      empty_hash[attribute] = []
+      value.each do | key, name_array |
+        name_array.each do | name_inside |
+          if name_inside == name 
+            empty_hash[attribute] << key.to_s 
+          end 
+        end 
+      end 
+    end 
+  end 
+  
+  new_hash
 end
+
+# pigeon_data = {
+#         :color => {
+#           :purple => ["Theo", "Peter Jr.", "Lucky"],
+#           :grey => ["Theo", "Peter Jr.", "Ms. K"],
+#           :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+#           :brown => ["Queenie", "Alex"]
+#         },
+#         :gender => {
+#           :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+#           :female => ["Queenie", "Ms. K"]
+#         },
+#         :lives => {
+#           "Subway" => ["Theo", "Queenie"],
+#           "Central Park" => ["Alex", "Ms. K", "Lucky"],
+#           "Library" => ["Peter Jr."],
+#           "City Hall" => ["Andrew"]
+#         }
